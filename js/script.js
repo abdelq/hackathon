aa8914f986f6ee72a7a8b038d03f87c9d8de2551
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2017-01-16 12:07:08 -0500

    Fixing some style stuff

--- js/script.js
+++ js/script.js
@@ -1,39 +1,39 @@
 $(function() {
-    var replays = [];
-    
-    $.get('../games.txt', function(data) {
-        var games = data.split('%');
+  var replays = [];
 
-        games.forEach(function(game, i) {
-            replays.push(game.split(/[^-]\n-+/));
-        });
+  $.get('games.txt', function(data) {
+    var games = data.split('%');
 
-        replays.sort(function() { return Math.random() < 0.5; });
-
-        var game = 0;
-        var frame = 0;
-        var $pre = $('#replay');
-        var first = replays[0][0].split('\n')[1];
-        
-        setInterval(function() {
-            $pre.text(replays[game][frame] + '|\n' + first);
-
-            frame++;
-
-            if(frame == replays[game].length - 1) {
-                frame = 0;
-                game = (game + 1) % replays.length;
-                var lines = replays[game][0].split('\n');
-                
-                first = lines[0] || lines[1];
-                console.log(replays[game][frame]);
-            }
-            
-        }, 130);
+    games.forEach(function(game, i) {
+      replays.push(game.split(/[^-]\n-+/));
     });
 
-    var press_start = $('.press-start');
+    replays.sort(function() { return Math.random() < 0.5; });
+
+    var game = 0;
+    var frame = 0;
+    var $pre = $('#replay');
+    var first = replays[0][0].split('\n')[1];
+
     setInterval(function() {
-        press_start.toggleClass('blink');
-    }, 600);
+      $pre.text(replays[game][frame] + '|\n' + first);
+
+      frame++;
+
+      if(frame == replays[game].length - 1) {
+        frame = 0;
+        game = (game + 1) % replays.length;
+        var lines = replays[game][0].split('\n');
+
+        first = lines[0] || lines[1];
+        console.log(replays[game][frame]);
+      }
+
+    }, 130);
+  });
+
+  var press_start = $('.press-start');
+  setInterval(function() {
+    press_start.toggleClass('blink');
+  }, 600);
 });
